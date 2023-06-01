a9ccbd0ace1e46eb76544e40d47a62b475e15d3e
joel071 <117740794+joel071@users.noreply.github.com>
2023-06-01 11:22:05 +0100

    Update Department.java

--- src/main/java/com/example/ExtractAndMapData/Entity/Department.java
+++ src/main/java/com/example/ExtractAndMapData/Entity/Department.java
@@ -1,4 +1,10 @@
 package com.example.ExtractAndMapData.Entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "tbl_dep")
+@Entity
 public class Department {
 }
